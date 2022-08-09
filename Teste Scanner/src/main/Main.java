30517ab1ff493d6343c45fb5b97656f7c384fd73
SilvaPedro11 <pedoca.360@gmail.com>
2022-08-09 19:45:59 -0300

    Projetos Java

---
+++ Teste Scanner/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Scanner grava = new Scanner (System.in);
+        double valor1, valor2, resultado;
+                
+        System.out.println("Digite o primeiro valor");
+        valor1 = grava.nextDouble();
+        
+        System.out.println("Digite o primeiro valor");
+        valor2 = grava.nextDouble();
+        
+        resultado = valor1 + valor2;
+        
+        System.out.println ("O resultado é \n " + resultado);
+                
+    
+    }
+    
+}
---
+++ Teste/src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        int tab =7;
+        int cont =1;
+        
+        //7x1 = 7
+        
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+        System.out.println(tab + " X " + cont + " = " + (tab*cont));
+        cont++;
+    }
+    
+}
